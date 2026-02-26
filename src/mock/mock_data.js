ac94411cdc8a158e8d492dfeb1ec6da97fe1e84f
CHINA-520 <meiyue365@outlook.com>
2026-02-26 13:32:47 +0800

    chore: 更新导航数据 - 2026/2/26 13:32:46

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -5,7 +5,29 @@ export const mockData = {
       "icon": "🔥",
       "name": "常用推荐",
       "order": 0,
-      "sites": []
+      "sites": [
+        {
+          "id": "site-1772083821807",
+          "name": "YouTube",
+          "url": "https://www.youtube.com",
+          "description": "在 YouTube 上畅享你喜爱的视频和音乐，上传原创内容并与亲朋好友和全世界观众分享你的视频。",
+          "icon": "https://www.faviconextractor.com/favicon/www.youtube.com?larger=true"
+        },
+        {
+          "id": "site-1772083887493",
+          "name": "Gmail邮箱",
+          "url": "https://mail.google.com/mail/u/0/#inbox",
+          "description": "Gmail 是一款直观、高效且实用的邮件应用。免费提供 15 GB 存储空间，具备出色的垃圾邮件过滤功能，而且支持移动访问。",
+          "icon": "https://www.faviconextractor.com/favicon/mail.google.com?larger=true"
+        },
+        {
+          "id": "site-1772083963165",
+          "name": "Google 表格",
+          "url": "https://docs.google.com/spreadsheets/u/0/",
+          "description": "Google 表格 是 Google 开发的在线电子表格应用，属于 Google Workspace（原 G Suite）的一部分。它与传统的 Excel 类似，但基于云端运行，支持多人实时协作和跨设备访问。",
+          "icon": "https://nav.778652.xyz/assets/img_1768042740172.png"
+        }
+      ]
     }
   ],
   "title": "猫猫导航🐱",
