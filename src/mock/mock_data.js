8b492d51bc949463b3b4ea2586f5a5097ac4e060
CHINA-520 <meiyue365@outlook.com>
2026-02-26 13:35:41 +0800

    chore: 更新导航数据 - 2026/2/26 13:35:39

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -26,6 +26,20 @@ export const mockData = {
           "url": "https://docs.google.com/spreadsheets/u/0/",
           "description": "Google 表格 是 Google 开发的在线电子表格应用，属于 Google Workspace（原 G Suite）的一部分。它与传统的 Excel 类似，但基于云端运行，支持多人实时协作和跨设备访问。",
           "icon": "https://nav.778652.xyz/assets/img_1768042740172.png"
+        },
+        {
+          "id": "site-1772084077804",
+          "name": "outlook邮箱",
+          "url": "https://www.microsoft.com/zh-cn/microsoft-365/outlook/email-and-calendar-software-microsoft-outlook?deeplink=%2Fmail%2F0%2F&sdf=0",
+          "description": "outlook是微软提供的免费电子邮件服务，既能满足个人通信需求，也能与微软生态系统深度整合，支持办公、云存储和跨设备同步。",
+          "icon": "https://ts4.mm.bing.net/th?id=OIP.8nOGS5-BwOFdIWG-GokNaQHaHa&pid=15.1"
+        },
+        {
+          "id": "site-1772084137535",
+          "name": "Dropbox",
+          "url": "https://www.dropbox.com/home",
+          "description": "Dropbox 是领先的云存储与协作平台，让你随时随地安全存储、同步、分享和编辑文件，支持 AI 智能搜索与团队高效工作。",
+          "icon": "https://www.faviconextractor.com/favicon/www.dropbox.com?larger=true"
         }
       ]
     }
